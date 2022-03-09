8e8c11413a9bb3fe0e34ddf8fa2d360794e92a0c
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-03-09 17:33:27 -0400

    Delete API_Test_CleanStruct directory

--- API_Test_CleanStruct/src/test/java/helpers/Configuration.java
+++
@@ -1,7 +0,0 @@
-package helpers;
-
-public class Configuration {
-    public static String host="https://todo.ly";
-    public static String user="paola_api@api.com";
-    public static String pwd="12345";
-}
